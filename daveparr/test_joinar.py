e74f4fba63fccef81bbe7cbf8d20a4b17a9adf54
dparr <dparr@continuummedia.tv>
2025-11-11 14:49:46 +0000

    feat(joinar): abstract column to coerce

--- daveparr/test_joinar.py
+++ daveparr/test_joinar.py
@@ -3,8 +3,8 @@ import polars as pl
 from polars.testing import assert_frame_equal
 
 def test_coerce_id_to_int():
-    result = joinar.coerce_id_to_int(pl.DataFrame({"character_id": [1.0, 2.0, 3.0]}))
-    assert_frame_equal(result, pl.DataFrame({"character_id": [1, 2, 3]}))
+    result = joinar.coerce_id_to_int(pl.DataFrame({"test_id": [1.0, 2.0, 3.0]}), "test_id")
+    assert_frame_equal(result, pl.DataFrame({"test_id": [1, 2, 3]}))
 
 def test_join_script_by_character():
     result = joinar.join_script_by_character(
